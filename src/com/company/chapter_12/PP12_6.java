3af7366c997da8fae5eff58df6d73954c604efb2
lebnor <liel4349@gmail.com>
2021-02-22 17:41:46 +0200

    Programming Project 12.6
    Signed-off-by: lebnor <liel4349@gmail.com>

---
+++ src/com/company/chapter_12/PP12_6.java
@@ -0,0 +1,57 @@
+package com.company.chapter_12;
+
+import java.util.Random;
+
+/**
+ * PP 12.6 In the language of an alien race, all words take the
+ * form of Blurbs. A Blurb is a Whoozit followed by one or more
+ * Whatzits. A Whoozit is the character ‘x’ followed by zero or
+ * more ‘y’s. A Whatzit is a ‘q’ followed by either a ‘z’ or a ‘d’,
+ * followed by a Whoozit. Design and implement a recursive
+ * program that generates random Blurbs in this alien language.
+ */
+public class PP12_6 {
+
+    private static final int MAX_LETTER = 10;
+
+    public static void main(String[] args) {
+
+        // TEST DRIVE
+        String blurb = generateBlurb();
+        System.out.println(blurb);
+    }
+
+    // return a Blurb
+    public static String generateBlurb() {
+        String whoozit = generateWhoozit();
+        String whatzit = generateWhatzit();
+        String endingWhoozit = generateWhoozit();
+        return  whoozit + whatzit + endingWhoozit;
+    }
+
+    // return a whatzit
+    private static String generateWhatzit() {
+        String whatzit = "q"; // starts with q
+        whatzit += new Random().nextBoolean() ? "z" : "d"; // add either z or d
+        return whatzit;
+    }
+    // return a whoozit
+    private static String generateWhoozit() {
+        String whoozit = "x"; // always starts with an x
+        whoozit += randomY(); // zero or more y
+        return whoozit;
+    }
+
+    // generate random amount of y's
+    private static String randomY() {
+        int max = (int) (Math.random() * MAX_LETTER);
+        return randomY("y", max);
+    }
+    // helper method
+    private static String randomY(String y , int max) {
+        if (y.length() >= max)
+            return y;
+        return randomY(y + "y", max);
+    }
+
+}
